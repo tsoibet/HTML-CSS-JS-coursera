c925e2f41646cc5f469ca9a71376924e1e46b0ce
tsoibet <bettsisai@gmail.com>
2021-01-05 12:45:48 +0900

    a little fix. completed

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -50,7 +50,7 @@ WARNING!!! WARNING!!!
     // string object's 'toLowerCase' method on the result so we can compare
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
-    var firstLetter = names[i][0].toLowerCase;
+    var firstLetter = names[i][0].toLowerCase();
 
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
